92b77d0a01f485abaa778b04ae0d966e1d2dd895
mprasanth3560 <prasanth.er15@gmail.com>
2023-08-17 11:09:39 +0530

    My Code

---
+++ src/org/git/MyDetails.java
@@ -0,0 +1,22 @@
+package org.git;
+
+public class MyDetails {
+		
+	public void name(String name) {
+		
+		System.out.println("My Name is :"+name);
+
+	}
+	
+	public void id(int value) {
+		System.out.println("My Id is :"+value);
+
+	}
+	public static void main(String[] args) {
+		
+		MyDetails md = new MyDetails();
+		md.name("krishna");
+		md.id(12345);
+	}
+
+}
